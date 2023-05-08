3848883006613da5542769ca0f6bcc39cdd021b8
Omar <omar25shaarawi@gmail.com>
2023-05-08 12:26:46 -0500

    Initial Commit

---
+++ Suhbah/SuhbahApp.swift
@@ -0,0 +1,20 @@
+//
+//  SuhbahApp.swift
+//  Suhbah
+//
+//  Created by Omar Shaarawi on 5/8/23.
+//
+
+import SwiftUI
+
+@main
+struct SuhbahApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
